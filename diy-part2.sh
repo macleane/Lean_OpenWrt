0c48a6bb4a869e79fbe9998cc56bc4c7c4db0101
macleane <macleane@163.com>
2021-07-25 10:57:03 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,6 +12,7 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/10.10.10.1/g' package/base-files/files/bin/config_generate
 git clone https://github.com/destan19/OpenAppFilter package/OpenAppFilter
 git clone https://github.com/Aslin-Ameng/luci-app-access-control package/luci-app-access-control
 git clone https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom package/luci-theme-infinityfreedom
